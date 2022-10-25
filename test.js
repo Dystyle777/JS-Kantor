df0c6232a304c8af45eb7bf05126808441ded786
Dystyle777 <dmitriylobanov15@gmail.com>
2022-10-25 13:58:24 +0300

    added pow *2 and *3

--- test.js
+++ test.js
@@ -1,14 +1,28 @@
 "use strict"
 // проверка возможных резултатов функции "pow"
 describe("pow", function () {
-   function makeTest(x) {
-        let expected = x * x * x;
-        it(`${x} встепени 3 будет ${expected}`, function(){
-            assert.equal(pow(x,3), expected);
-        });
-   }
-   for (let x = 1; x<= 5; x++) {
-    makeTest(x);
-    console.log(x);
-   }
+    describe("возводит x в степень 2", function () {
+        function makeTest(x) {
+            let expected = x * x;
+            it(`${x} в степени 2 будет ${expected}`, function () {
+                assert.equal(pow(x, 2), expected);
+            });
+        }
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+            console.log(x);
+        }
+    });
+    describe("возводит x в степень 3", function () {
+        function makeTest(x) {
+            let expected = x * x * x;
+            it(`${x} в степени 3 будет ${expected}`, function () {
+                assert.equal(pow(x, 3), expected);
+            });
+        }
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+            console.log(x);
+        }
+    });
 });
