cb01f5f30177a0e5fdc413706d1ea232e224ba33
Dystyle777 <dmitriylobanov15@gmail.com>
2022-10-26 09:04:07 +0300

    object 'start'

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-"use strict"
+// "use strict"
 
 let obj = {};
-console.log(obj);
+console.log('Объект obj ' + obj);
+
+let obj2 = new Object();
+console.log('Объект obj2 ' + obj2);
+
+let user = {
+    name: 'John',
+    age: 30,
+    height: '167cm',
+    "like video games": true //имя может содержать 2 слова - нужно заключить в кавычки
+}
+
+console.log(user); // show object
+console.log(user.name); // show element object's
+console.log(user.height); 
+console.log(user.age);
+
+user.isAdmin = true; // added element
+delete user.age;
+console.log(user);
+
