691f17448783a0216f8198b170aabd79d8e9ea43
Dystyle777 <dmitriylobanov15@gmail.com>
2022-10-27 17:53:50 +0300

    property of object

--- script.js
+++ script.js
@@ -116,21 +116,85 @@
 // schedule["8: 30"] = "get up";
 // alert( isEmpty(schedule) ); // false
 
-function isEmpty(obj) {
-    for ( let prop in obj) {
-        // если тело цикла начнет выполняться - значит в объекте есть свойства
-        return false;
+// function isEmpty(obj) {
+//     for ( let prop in obj) {
+//         // если тело цикла начнет выполняться - значит в объекте есть свойства
+//         return false;
+//     }
+//     return true;
+// }
+
+// let shedule = {
+//     name: 'Max',
+//     'get up': '8:30',
+//     age: 30,
+// }
+
+// // если объект:
+// //              пуст - true
+// //              заполнен - false
+// console.log( isEmpty( shedule ));
+
+// #3 Сумма свойств объекта
+// У нас есть объект, в котором хранятся зарплаты нашей команды:
+// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum .
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+//     }
+// Должно получиться 390 .
+// Если объект salaries пуст, то результат должен быть 0 .
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// };
+// let sum = 0;
+
+// for (let key in salaries) {
+//     sum += salaries[key];
+// } 
+// alert(sum); // 390
+
+// #4 Умножаем все числовые свойства на 2
+// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства
+// объекта obj на 2.
+//     Например:
+
+// // до вызова функции
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+// multiplyNumeric(menu);
+// // после вызова функции
+// menu = {
+//     width: 400,
+//     height: 600,
+//     title: "My menu"
+// };
+//     Обратите внимание, что multiplyNumeric не нужно ничего возвращать.Следует
+// напрямую изменять объект.
+//     P.S.Используйте typeof для проверки, что значение свойства числовое.
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
     }
-    return true;
-}
+};
 
-let shedule = {
-    name: 'Max',
-    'get up': '8:30',
-    age: 30,
-}
+console.log(menu); // до применения ф-ии
+multiplyNumeric(menu);
 
-// если объект:
-//              пуст - true
-//              заполнен - false
-console.log( isEmpty( shedule ));
+console.log(menu); // после применения ф-ии
