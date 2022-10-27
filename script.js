d0f2738ed46a6c7f21373a486fed20dafcdafde9
Dystyle777 <dmitriylobanov15@gmail.com>
2022-10-27 16:18:28 +0300

    property of object

--- script.js
+++ script.js
@@ -1,24 +1,72 @@
-// "use strict"
+"use strict"
+// -=-=-=-=-=-- создание оюъекта и обращение к элементам объекта
+// let obj = {};
 
-let obj = {};
-console.log('Объект obj ' + obj);
+// let obj2 = new Object();
 
-let obj2 = new Object();
-console.log('Объект obj2 ' + obj2);
+// let user = {
+//     name: 'John',
+//     age: 30,
+//     height: '167cm',
+//     name_param: "игроман" //имя может содержать 2 слова - нужно заключить в кавычки
+// }
 
-let user = {
-    name: 'John',
-    age: 30,
-    height: '167cm',
-    "like video games": true //имя может содержать 2 слова - нужно заключить в кавычки
-}
+// let name_param = "like game"
 
-console.log(user); // show object
-console.log(user.name); // show element object's
-console.log(user.height); 
-console.log(user.age);
+// console.log(user); // show object
+// console.log(user.name); // show element object's
+// console.log(user.height); 
+// console.log(user.age);
 
-user.isAdmin = true; // added element
-delete user.age;
-console.log(user);
+// user.isAdmin = true; // added element
+// delete user.age;
 
+// console.log(user);
+// console.log(user[name_param]);
+
+// let timeVar = "time var";
+// user[timeVar] = true;
+
+// alert(user[timeVar]);
+
+// -=-=-=-=-=-=- применение объектов в функциях
+
+// function makeUser(name, age) {
+//     return {
+//         name,
+//         age
+//     }
+// }
+
+// let user = makeUser('Max', 25);
+// alert (user.age);
+// alert (user.name);
+// console.log(user);
+
+// =-=-=-=-=-=-=-= особенности объектов
+// let obj = {
+//     0: "Тест" // то же самое что и "0": "Тест"
+// };
+// обе функции alert выведут одно и то же свойство (число 0 преобразуется в строку "0")
+// alert(obj["0"]); // Тест
+// alert(obj[0]); // Тест (то же свойство)
+
+//                         2nd variant
+// let user = {};
+// alert( user.noSuchProperty === undefined ); // true означает "свойства нет"
+// console.log(user);
+
+// if (user.noSuchProperty == undefined) {
+//     console.log('hi');
+// }
+// else {
+//     console.log('by');
+// }
+
+// =-=-=-=-=-=-=-= оператор in
+// let user = {name:'Max', age:30,}
+
+// alert ('name' in user);
+// alert ('asdadad' in user);
+
+// -=-=-=-=- Цикл «for…in»
