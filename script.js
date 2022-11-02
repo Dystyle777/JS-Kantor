1c7485d35d439f7f0fb41ef85abb8637adf725a3
Dystyle777 <dmitriylobanov15@gmail.com>
2022-11-02 06:58:47 +0300

    this

--- script.js
+++ script.js
@@ -75,11 +75,126 @@
 
 // У стрелочных функций нет «this»
 
-let user = {
-    firstName: "Илья",
-    sayHi() {
-        let arrow = () => alert(this.firstName);
-        arrow();
+// let user = {
+//         firstName: "Илья",
+//     sayHi() {
+//         let arrow = () => alert(this.lastName);
+//         arrow();
+//     },
+//     lastName: "Lapin"
+// };
+// user.sayHi(); // Илья
+
+
+// практика 
+// #1 Каким будет результат выполнения этого кода?
+// let user = {
+//     name: "Джон",
+//     go: function () { alert(this.name) }
+// };
+// (user.go)()
+// выведет ошибку из-за отсутствия ; после } переде (
+
+// #2 Объясните значение "this"
+// Впредставленном ниже коде мы намерены вызвать obj . go( ) метод 4 раза подряд.
+// Но вызовы ( 1) и ( 2) работают иначе, чем ( 3) и ( 4) . Почему?
+// let obj, method;
+// obj = {
+//     go: function() { alert(this); }
+// };
+// obj.go(); // (1) [obj ect Obj ect] 
+// (obj.go)(); // (2) [obj ect Obj ect]
+// (method = obj.go)(); // (3) undefined --- Здесь f( ) выполняется как функция, без передачи значения 
+// (obj.go || true)(); // (4) undefined    -----Тут похожая ситуация на случай ( 3) – идёт потеря значения this .
+//                                             // Чтобы объяснить поведение в примерах ( 3) и ( 4) , нам нужно помнить, что
+//                                             // доступ к свойству (через точку или квадратные скобки) возвращает специальное
+//                                             // значение ссылочного типа (Reference Type).
+//                                             // За исключением вызова метода, любая другая операция (подобно операции
+//                                             // присваивания = или сравнения через логические операторы, например | | )
+//                                             // превращает это значение в обычное, которое не несёт информации, позволяющей
+//                                             // установить this .
+
+// #3 Использование "this" в литерале объекта
+// есь функция makeUser возвращает объект.
+// Каким будет результат при обращении к свойству объекта ref ? Почему?
+// function makeUser() {
+//     return {
+//         name: "Джон",
+//         ref: this
+//     };
+// };
+
+// let user = makeUser();
+// console.log(makeUser());
+// alert(  user.ref.name ) // Каким будет результат? --- Cannot read properties of undefined
+
+// #4 Создайте калькулятор
+// Создайте объект calculator (калькулятор) с тремя методами:
+//  - read( ) (читать) запрашивает два значения и сохраняет их как свойства объекта.
+//  - sum( ) (суммировать) возвращает сумму сохранённых значений.
+//  - mul( ) (умножить) перемножает сохранённые значения и возвращает результат
+
+// let calculator = {
+//     sum() {
+//         return this.a + this.b;
+//     },
+//     mul() {
+//         return this.a * this.b;
+//     },
+//     read() {
+//         this.a = prompt('a?', 0);
+//         this.b = prompt('b?', 0);
+//     },
+// };
+// calculator.read();
+// console.log('a = ' + calculator.a);
+// console.log('b = ' + calculator.b);
+// console.log('Sum = ' + calculator.sum());
+// console.log('Mul = ' + calculator.mul());
+
+
+// #5  Цепь вызовов
+// Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
+// let ladder = {
+//     step: 0,
+//     up() {
+//         this.step++;
+//     },
+//     down() {
+//         this.step--;
+//     },
+//     showStep: function () { // показывает текущую ступеньку
+//         console.log(this.step);
+//     }
+// };
+// // Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем
+// // выполнить это так:
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+
+// Измените код методов up , down и showStep таким образом, чтобы их вызов можно
+// было сделать по цепочке, например так:
+// ladder.up().up().down().showStep(); // 1
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        console.log(this.step);
+        return this;
     }
 };
-user.sayHi(); // Илья
+ladder
+    .up()
+    .up()
+    .down()
+    .showStep();
+
